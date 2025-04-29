b30d16a013f033ad4bb57bde1fbf35c79f93de88
luizzzmn <luizmanoel10098@gmail.com>
2025-04-29 10:07:30 -0300

    feat: projeto funcionando melhor do que eu queria. ass. Vitor no PC de Luiz

--- src/cubo3d/teste3d.py
+++ src/cubo3d/teste3d.py
@@ -2,6 +2,8 @@ import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
+import threading
+import time
 
 from algorithms3d.translacao3d import translacao3d
 from algorithms3d.cisalhamento3d import cisalhamento3d
@@ -9,6 +11,20 @@ from algorithms3d.rotacao3d import rotacao3d
 from drawCube import desenhar_eixos, desenhar_cubo, gerar_cubo_na_origem
 from algorithms3d.escala3d import escala3d
 from algorithms3d.reflexao3d import reflexao3d
+
+from menu import menu_transformacoes
+
+# Lista de transformações acumuladas
+transformacoes = []
+
+def executar_menu_em_loop():
+    while True:
+        tipo, parametros = menu_transformacoes()
+        if tipo == 'sair':
+            print("Encerrando entrada de transformações.")
+            break
+        transformacoes.append((tipo, parametros))
+        print(f"[INFO] Transformação '{tipo}' adicionada.\n")
 
 def inicializar_janela(largura, altura, titulo):
     if not glfw.init():
@@ -28,32 +44,48 @@ def definir_camera():
     glLoadIdentity()
     gluLookAt(5, 5, 5, 0, 0, 0, 0, 1, 0)
 
-def main():
-    janela = inicializar_janela(1600, 900, "Cubo na Origem")
+def aplicar_transformacoes(vertices):
+    for tipo, parametros in transformacoes:
+        if tipo == 'escala':
+            vertices = escala3d(vertices, parametros)
+        elif tipo == 'translacao':
+            vertices = translacao3d(vertices, parametros)
+        elif tipo == 'reflexao':
+            vertices = reflexao3d(vertices, parametros)
+        elif tipo == 'cisalhamento':
+            sh_xy, sh_xz, sh_yz = parametros
+            vertices = cisalhamento3d(vertices, sh_xy, sh_xz, sh_yz)
+        elif tipo == 'rotacao':
+            angulo, eixo = parametros
+            vertices = rotacao3d(vertices, angulo, eixo)
+    return vertices
 
+def main():
+    # Inicia o menu em thread paralela
+    menu_thread = threading.Thread(target=executar_menu_em_loop, daemon=True)
+    menu_thread.start()
+
+    # Inicia janela e cubo
+    janela = inicializar_janela(1600, 900, "Cubo Interativo")
     glEnable(GL_DEPTH_TEST)
+
+    lado = 2
+    vertices_base = gerar_cubo_na_origem(lado)
 
     while not glfw.window_should_close(janela):
         glfw.poll_events()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         definir_camera()
-
         desenhar_eixos()
 
-        lado = 2
-        vertices = gerar_cubo_na_origem(lado)
-        fatores_escala = (1.0, 1.0, 0.5)
-
-        # vertices = escala3d(vertices, fatores_escala)
-        # vertices = translacao3d(vertices, fatores_escala)
-        # vertices = escala3d(vertices, fatores_escala)
-        # vertices = reflexao3d(vertices, 'yz')
-        # vertices = cisalhamento3d(vertices, sh_xy=0.5, sh_xz=0.2, sh_yz=0.3)
-        vertices = rotacao3d(vertices, 0, eixo='x')
+        vertices = np.copy(vertices_base)
+        vertices = aplicar_transformacoes(vertices)
 
         desenhar_cubo(vertices)
-
         glfw.swap_buffers(janela)
+
+        # Pequeno delay para reduzir uso de CPU
+        time.sleep(0.01)
 
     glfw.terminate()
 
